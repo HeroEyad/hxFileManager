e06a512baa7215740eca5fab0fc92bc7a4774276
Hero <68159082+HeroEyad@users.noreply.github.com>
2025-07-01 02:12:35 +0300

    doing this later

--- hxFileManager/FileUtils.hx
+++ hxFileManager/FileUtils.hx
@@ -1,7 +1,35 @@
 package hxFileManager;
 
-@:headerCode('#include <windows.h>\nextern "C" bool is_user_admin();')
-@:cppFileCode('extern "C" bool is_user_admin();')
+@:headerCode('
+    #include <windows.h>
+
+    extern "C" bool is_user_admin();
+')
+
+@:cppFileCode('
+    extern "C" bool is_user_admin() {
+        BOOL fIsElevated = FALSE;
+        HANDLE hToken = NULL;
+        TOKEN_ELEVATION elevation;
+        DWORD dwSize;
+
+        if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+            return false;
+        }
+
+        if (!GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize)) {
+            CloseHandle(hToken);
+            return false;
+        }
+
+        fIsElevated = elevation.TokenIsElevated;
+        CloseHandle(hToken);
+        return fIsElevated ? true : false;
+    }
+')
+
 extern class FileUtils {
+    @:native("is_user_admin")
     public static function isUserAdmin():Bool;
 }
+
